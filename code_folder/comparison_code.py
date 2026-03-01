62c3a431b8ec992997298f09f97ccd12408ea2e4
Britney01234 <bcleme01@student.bbk.ac.uk>
2026-03-01 15:47:32 +0000

    had to include a very long pathway

--- code_folder/comparison_code.py
+++ code_folder/comparison_code.py
@@ -1,4 +1,4 @@
-def compare_sequences(dog_breed_database='../data_folder/dog_breeds.fa',mystery_dog_sequence='../data_folder/mystery.fa'):
+def compare_sequences(dog_breed_database='C:\Users\15bcl\Documents\Bioinformatics\Biocomputing\Project Files\project_dog_dna\project_dog_dna\dog_breeds.fa',mystery_dog_sequence='C:\Users\15bcl\Documents\Bioinformatics\Biocomputing\Project Files\project_dog_dna\project_dog_dna\mystery.fa'):
 # Defines variable as an empty dictionary to store the dog breed information from the database.
     dog_dictionary = {}
 # Opens the dog breed database file. 
