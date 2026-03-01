f51cd251ac7c61afeb928e2aba9f90985d2441bc
Britney01234 <bcleme01@student.bbk.ac.uk>
2026-03-01 15:56:44 +0000

    better names for def

--- code_folder/comparison_code.py
+++ code_folder/comparison_code.py
@@ -40,7 +40,7 @@ def dictionary_dogs(dog_breed_database='C:\\Users\\15bcl\\Documents\\Bioinformat
 
     return mystery_str, dog_dictionary
 
-def compare_sequences(input_mystery_str, input_dog_dictionary):
+def compare_sequences(mystery_sequence_for_comparison, dog_dictionary_for_comparison):
 
 
 dictionary_dogs()
